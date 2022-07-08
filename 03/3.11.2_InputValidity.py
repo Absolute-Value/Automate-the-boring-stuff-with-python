98b04528ca7f48349f8396fc41130da59a23aacc
Absolute-Value <ji.kei1220@gmail.com>
2022-07-09 01:10:06 +0900

    update 03

---
+++ 03/3.11.2_InputValidity.py
@@ -0,0 +1,19 @@
+def collatz(number):
+    if number % 2 == 0:
+        return number / 2
+    else:
+        return 3 * number + 1
+
+while(True):
+    try:
+        number = int(input("整数を入力してください："))
+        break
+    except:
+        print("エラー：整数値を入力してください")
+
+while(True):
+    print(number)
+    if number == 1:
+        break
+    else:
+        number = int(collatz(number))
