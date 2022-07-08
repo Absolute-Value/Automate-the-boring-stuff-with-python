54b291fecb92144b532aaf6b41c9a7fb21896a2f
Absolute-Value <ji.kei1220@gmail.com>
2022-07-09 02:29:34 +0900

    Add 06

---
+++ 06/6.7.1_ShowTable.py
@@ -0,0 +1,20 @@
+# 6.7.1 表の表示
+
+def print_table(table_data):
+    col_widths = [0] * len(table_data)
+    for i, datas in enumerate(table_data):
+        for data in datas:
+            length = len(data)
+            if length > col_widths[i]:
+                col_widths[i] = length
+                
+    for i in range(len(table_data[0])):
+        for j in range(len(table_data)):
+            print(table_data[j][i].rjust(col_widths[j]), end='')
+            print(' ',end='')
+        print('')
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+              ['Alice', 'Bob', 'Carol', 'David'],
+              ['dogs', 'cats', 'moose', 'goose']]
+print_table(table_data)
