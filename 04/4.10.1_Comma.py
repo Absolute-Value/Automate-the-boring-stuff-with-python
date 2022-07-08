909eacd7abfdda02666ce2080183f7284297ada2
Absolute-Value <ji.kei1220@gmail.com>
2022-07-09 01:23:42 +0900

    update 04

---
+++ 04/4.10.1_Comma.py
@@ -0,0 +1,11 @@
+# 4.10.1 コンマ付け
+
+def AddComma(given_list):
+    string = ''
+    for _ in range(len(given_list) - 1):
+        string += given_list.pop(0) + ', '
+    string += 'and ' + given_list[0]
+    return string
+
+spam = ['apples', 'bananas', 'tohu', 'cats']
+print(AddComma(spam))
