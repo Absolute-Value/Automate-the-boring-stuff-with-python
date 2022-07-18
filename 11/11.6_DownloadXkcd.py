8046d495d9556624fc35a824f1e2ce2976ce354d
Absolute-Value <ji.kei1220@gmail.com>
2022-07-19 04:03:57 +0900

    Update

--- 11/11.6_DownloadXkcd.py
+++ 11/11.6_DownloadXkcd.py
@@ -9,7 +9,7 @@ os.makedirs('xkcd', exist_ok=True)    # ./xkcdに保存する
 
 while not url.endswith('#'):
     # ページをダウンロードする
-    print('ページをダウンロード中 {}...'.format(url))
+    print(f'ページをダウンロード中 {url}...')
     res = requests.get(url)
     res.raise_for_status()
 
@@ -22,15 +22,14 @@ while not url.endswith('#'):
     else:
         comic_url = 'http:' + comic_elem[0].get('src')
         # 画像をダウンロードする
-        print('画像をダウンロード中 {}...'.format(comic_url))
+        print(f'画像をダウンロード中 {comic_url}...')
         res = requests.get(comic_url)
         res.raise_for_status()
 
         # 画像を./xkcdに保存する
-        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
-        for chunk in res.iter_content(100000):
-            image_file.write(chunk)
-        image_file.close()
+        with open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb') as image_file:
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
         
     # PrevボタンのURLを取得する
     prev_link = soup.select('a[rel="prev"]')[0]
