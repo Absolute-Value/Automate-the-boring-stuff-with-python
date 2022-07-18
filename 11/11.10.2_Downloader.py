d015959af778fbb522256df98e57c8bd98502fa6
Absolute-Value <ji.kei1220@gmail.com>
2022-07-19 04:13:07 +0900

    Update

--- 11/11.10.2_Downloader.py
+++ 11/11.10.2_Downloader.py
@@ -2,9 +2,9 @@
 # 11.10.2_Downloader.py - 画像サイトのダウンローダー
 
 import os
-from PIL import Image
-from io import BytesIO
 import requests, sys, bs4, re
+
+DIR = 'downloads'
 
 url = 'https://www.flickr.com/search/?text=' + ' '.join(sys.argv[1:])
 print(f'ページをダウンロード中 {url}...') # Googleページをダウンロード中にテキストを表示
@@ -18,14 +18,14 @@ regex = re.findall(r'//.*\.jpg', str(link_elems))
 if len(regex) == 0:
     print('画像が見つかりませんでした。')
 else:
-    os.makedirs('downloads', exist_ok=True)
+    os.makedirs(DIR, exist_ok=True)
     for i, reg in enumerate(regex):
         img_url = 'https:' + reg
         print(f'画像をダウンロード中 {img_url}...')
         res = requests.get(img_url)
         res.raise_for_status()
 
-        with open(os.path.join('downloads', f'{i+1}_{os.path.basename(img_url)}'), 'wb') as image_file:
+        with open(os.path.join(DIR, f'{i+1}_{os.path.basename(img_url)}'), 'wb') as image_file:
             for chunk in res.iter_content(100000):
                 image_file.write(chunk)
 
