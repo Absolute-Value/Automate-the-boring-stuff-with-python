3cd8a0dc06b04d0b6258a122985e87cff1b1a3b2
Absolute-Value <ji.kei1220@gmail.com>
2022-07-19 03:58:42 +0900

    Update 11.10.2

--- 11/11.10.2_Downloader.py
+++ 11/11.10.2_Downloader.py
@@ -6,22 +6,27 @@ from PIL import Image
 from io import BytesIO
 import requests, sys, bs4, re
 
-print('Googling...') # Googleページをダウンロード中にテキストを表示
-res = requests.get('https://www.flickr.com/search/?text=' + ' '.join(sys.argv[1:]))
+url = 'https://www.flickr.com/search/?text=' + ' '.join(sys.argv[1:])
+print(f'ページをダウンロード中 {url}...') # Googleページをダウンロード中にテキストを表示
+res = requests.get(url)
 res.raise_for_status()
 
 # 上位の検索結果のリンクを取得する
 soup = bs4.BeautifulSoup(res.text, 'html5lib')
 link_elems = soup.select(".view .awake")
-
 regex = re.findall(r'//.*\.jpg', str(link_elems))
-
-if len(regex) > 0:
+if len(regex) == 0:
+    print('画像が見つかりませんでした。')
+else:
     os.makedirs('downloads', exist_ok=True)
     for i, reg in enumerate(regex):
-        print(reg)
-        res = requests.get('https:' + reg)
-        if res.status_code == 200:
-            with BytesIO(res.content) as buf:
-                im = Image.open(buf)             # PIL.Imageで読込む
-                im.save(f"downloads/{sys.argv[1]}_{i}.{im.format.lower()}") # 保存ファイル名に拡張子を設定
+        img_url = 'https:' + reg
+        print(f'画像をダウンロード中 {img_url}...')
+        res = requests.get(img_url)
+        res.raise_for_status()
+
+        with open(os.path.join('downloads', f'{i+1}_{os.path.basename(img_url)}'), 'wb') as image_file:
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
+
+print('完了')
