18e7ae63941ca467eae759c50012a10b64dfc748
Absolute-Value <ji.kei1220@gmail.com>
2022-07-18 19:44:19 +0900

    Update 9.7.1

--- 09/9.7.1_SelectCopy.py
+++ 09/9.7.1_SelectCopy.py
@@ -3,43 +3,29 @@
 # Usage:
 # 9.4_BackupToZip.py <folder> <extention> - <folder>の<extention(拡張子)>を新しいフォルダにコピー
 
-import zipfile, os, sys
+import shutil, os, sys
 
 # クリップボードの内容を保存
 if len(sys.argv) != 3:
     print("コピーにしたいフォルダと拡張子を入力してください")
 
-def backup_to_zip(folder):
+def backup_to_zip(folder, extention):
     #folder = os.path.abspath(folder) # folderを絶対パスに
-
-    # 既存のファイル名からファイル名の連番を決める
-    number = 1
-    while True:
-        zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
-        if not os.path.exists(zip_filename):
-            break
-        number += 1
-
-    # ZIPファイルを作成する
-    print(f'Creating {zip_filename}...')
-    backup_zip = zipfile.ZipFile(zip_filename, 'w')
 
     # フォルダのツリーを渡り歩いてその中のファイルを圧縮する
     for foldername, subfolders, filenames in os.walk(folder):
-        foldername2 = foldername.replace('..','.')
+        new_foldername = foldername.replace('..','.')
 
-        print(f'Adding files in {foldername2}')
-        # 現在のフォルダをZIPファイルに追加する
-        #backup_zip.write(foldername)
         # 現在のフォルダの中の全ファイルをZIPファイルに追加する
         for filename in filenames:
-            new_base = os.path.basename(folder) + '_'
-            print(new_base)
-            if filename.startswith(new_base) and filename.endswith('.zip'):
-                continue
-            backup_zip.write(os.path.join(foldername, filename), os.path.join(foldername2,filename))
+            if str(extention) in filename:
+                os.makedirs(new_foldername, exist_ok=True)
+                print(f'Making folder: {new_foldername}')
 
-    backup_zip.close()
+                newfile_path = os.path.join(new_foldername,filename)
+                shutil.copy(os.path.join(foldername, filename), newfile_path)
+                print(f'Copy file: {newfile_path}')
+
     print("Done.")
 
-backup_to_zip(sys.argv[1])
+backup_to_zip(sys.argv[1], sys.argv[2])
